e69caaab39ca788436d2cc7ad16547774b82cb4f
qisan <yimiqisan@gmail.com>
2015-11-05 22:01:45 +0800

    microsite launch

--- src/suibe/views/__init__.py
+++ src/suibe/views/__init__.py
@@ -1,2 +1,9 @@
 # -*- code: utf-8 -*-
 from __future__ import unicode_literals
+
+from . import index     # noqa pyflakes:ignore
+from . import channel   # noqa pyflakes:ignore
+from . import article   # noqa pyflakes:ignore
+
+
+__all__ = ['index', 'channel', 'article']
